5d2e28014808084b886ca68e9ad3da81965633cf
Carsonwl <carson.long@gmail.com>
2023-10-07 10:32:25 -0500

    working video selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import React from "react";
 import { useState } from "react";
 import { useRef } from "react";
 import { Button, FormGroup } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import ReactPlayer from "react-player/youtube";
 import VideoFrame from "./components/VideoFrame";
+
+
 
 function App() {
   const workURL = useRef("https://www.youtube.com/watch?v=jfKfPfyJRdk");
@@ -17,66 +22,81 @@ function App() {
 
   function getURL(e) {
     e.preventDefault();
+    // grab URL of form related to button
     let url = e.target.previousSibling.value;
+
+    // grab data from button to determine if it targets work or fun video
     let targetType = e.target.attributes.data.value;
-    console.log(`URL is ${url} and target is ${targetType}`)
-    
+
+    // console.log(`URL is ${url} and target is ${targetType}`)
+
     if (targetType === "workSubmit" && ReactPlayer.canPlay(url)) {
       setworkVideo(url);
-    }
-    
-    else if (targetType === "funSubmit" && ReactPlayer.canPlay(url)) {
-      setfunVideo(url); 
-    }
-    else {
+    } else if (targetType === "funSubmit" && ReactPlayer.canPlay(url)) {
+      setfunVideo(url);
+    } else {
       alert("Invalid Youtube URL");
-    };
+    }
   }
 
   return (
     <>
-      <div className='workContainer'>
-        <FormGroup>
-          <Form.Label>Focus Video: </Form.Label>
-          <Form.Control
-            className='workURLInput'
-            type='input'
-            defaultValue={workURL.current}
-          />
-          <Button
-            type='submit'
-            variant='primary'
-            onClick={getURL}
-            data = "workSubmit"
-          >
-            Submit
-          </Button>
-        </FormGroup>
-        <div className='videoContainer'>
-          <VideoFrame url={workVideo} />
-        </div>
-      </div>
-      <div className='funContainer'>
-        <FormGroup>
-          <Form.Label>Fun Video: </Form.Label>
-          <Form.Control
-            className='funURLInput'
-            type='input'
-            defaultValue={funURL.current}
-          />
-          <Button
-            type='submit'
-            variant='primary'
-            onClick={getURL}
-            data="funSubmit"
-          >
-            Submit
-          </Button>
-        </FormGroup>
-        <div className='videoContainer'>
-          <VideoFrame url={funVideo} />
-        </div>
-      </div>
+      <Container fluid>
+        <Row>
+          <h1>Pomodoro TV</h1>
+          <p>Use Pomodoro Timing to alternate playing different videos.</p>
+        </Row>
+        <Row>
+          <Col>
+            <div className='workContainer'>
+              <FormGroup>
+                <Form.Label>Focus Video: </Form.Label>
+                <Form.Control
+                  className='workURLInput'
+                  type='input'
+                  defaultValue={workURL.current}
+                />
+                <Button
+                  type='submit'
+                  variant='primary'
+                  onClick={getURL}
+                  data='workSubmit'
+                  className="m-2"
+                >
+                  Submit
+                </Button>
+              </FormGroup>
+              <div className='videoContainer'>
+                <VideoFrame url={workVideo} />
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className='funContainer'>
+              <FormGroup>
+                <Form.Label>Fun Video: </Form.Label>
+                <Form.Control
+                  className='funURLInput'
+                  type='input'
+                  defaultValue={funURL.current}
+                />
+                <Button
+                  type='submit'
+                  variant='primary'
+                  onClick={getURL}
+                  data='funSubmit'
+                  className="m-2"
+                >
+                  Submit
+                </Button>
+              </FormGroup>
+              <div className='videoContainer'>
+                <VideoFrame url={funVideo} />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
