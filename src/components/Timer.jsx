ec86cb4ec18c36c96dcf2b63ab27eaf4bb1ac112
Carsonwl <carson.long@gmail.com>
2023-10-12 12:52:40 -0500

    Slider working. Refactoring timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -22,39 +22,12 @@ class Timer extends Component {
           } = useTimer({
             this.state.countdown,
             autoStart: false,
-            onExpire: () => this.toggleVideo(),
           });
     }
 
     render() { 
-        return (  );
-    }
-}
- 
-export default Timer;
-
-// REFERENCE
-
-function VidTimer({ expirytimestamp }) {
-    const {
-      totalSeconds,
-      seconds,
-      minutes,
-      hours,
-      days,
-      isRunning,
-      start,
-      pause,
-      resume,
-      restart,
-    } = useTimer({
-      expirytimestamp,
-      autoStart: false,
-      onExpire: () => toggleVideo(),
-    });
-
-    return (
-      <div style={{ textAlign: "center" }}>
+        return ( 
+            <div style={{ textAlign: "center" }}>
         <p>Pomodoro Timer</p>
         <div style={{ fontSize: "100px" }}>
           <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
@@ -74,5 +47,52 @@ function VidTimer({ expirytimestamp }) {
           Restart
         </button>
       </div>
-    );
-  }
+         );
+    }
+}
+ 
+export default Timer;
+
+// REFERENCE
+
+// function VidTimer({ expirytimestamp }) {
+//     const {
+//       totalSeconds,
+//       seconds,
+//       minutes,
+//       hours,
+//       days,
+//       isRunning,
+//       start,
+//       pause,
+//       resume,
+//       restart,
+//     } = useTimer({
+//       expirytimestamp,
+//       autoStart: false,
+//       onExpire: () => toggleVideo(),
+//     });
+
+//     return (
+//       <div style={{ textAlign: "center" }}>
+//         <p>Pomodoro Timer</p>
+//         <div style={{ fontSize: "100px" }}>
+//           <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+//         </div>
+//         <p>{isRunning ? "Running" : "Not running"}</p>
+//         <button onClick={start}>Start</button>
+//         <button onClick={pause}>Pause</button>
+//         <button onClick={resume}>Resume</button>
+//         <button
+//           onClick={() => {
+//             // Restarts to 5 minutes timer
+//             const time = new Date();
+//             time.setSeconds(time.getSeconds() + 10);
+//             restart(time);
+//           }}
+//         >
+//           Restart
+//         </button>
+//       </div>
+//     );
+//   }
